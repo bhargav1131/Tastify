921c4b6b524c5bde1590f3386cee401461ebef28
Bhargav Pratim <bhargavsarma173@gmail.com>
2024-07-19 14:30:59 +0530

    add some dummy categories and blueprint for the categories

---
+++ lib/models/category_mod.dart
@@ -0,0 +1,14 @@
+// blueprint for category
+import 'package:flutter/material.dart';
+
+class Category{
+  const Category({
+    required this.id, 
+    required this.title, 
+    this.color = const Color.fromARGB(255, 162, 215, 101)
+  });
+
+  final String id;
+  final String title;
+  final Color color;
+}
