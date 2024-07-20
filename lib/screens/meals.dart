8d4998e91d8573f569b95c5ca4a023770ce4dd29
Bhargav Pratim <bhargavsarma173@gmail.com>
2024-07-20 21:08:11 +0530

    add logic to handle meals desc

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tastify/models/meal_mod.dart';
 
-class MealsScreen extends StatelessWidget{
+class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meals});
 
   final String title;
@@ -9,11 +9,41 @@ class MealsScreen extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: ,
+    Widget content = ListView.builder(
+      itemCount: meals.length,
+      itemBuilder: (ctx, index) => Text(meals[index].title),
     );
+
+    if (meals.isEmpty) {
+      content = Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Uh oh ..Nothing here!',
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.onSurface),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Text(
+              'try selecting a different category',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.onSurface),
+            )
+          ],
+        ),
+      );
+    }
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: content);
   }
 }
