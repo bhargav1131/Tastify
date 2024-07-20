ecc9a157e14810837577d3c9d0d06f5b6a378a66
Bhargav Pratim <bhargavsarma173@gmail.com>
2024-07-20 20:45:16 +0530

    modify recipe screen file

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:tastify/models/meal_mod.dart';
 
 class MealsScreen extends StatelessWidget{
-  const MealsScreen({super.key});
+  const MealsScreen({super.key, required this.title, required this.meals});
+
+  final String title;
+  final List<Meal> meals;
 
   @override
   Widget build(BuildContext context) {
-    
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: ,
+    );
   }
 }
