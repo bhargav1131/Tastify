bcc863c936dd1c42d86bf42586b0c9c3b56d253d
Bhargav Pratim <bhargavsarma173@gmail.com>
2024-07-22 21:02:53 +0530

    show affordbility, complexity and duration in meta data

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tastify/models/meal_mod.dart';
+import 'package:tastify/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
@@ -9,6 +10,13 @@ class MealItem extends StatelessWidget {
   });
 
   final Meal meal;
+
+  String get complexityText{
+    return meal.complexity.name[0].toUpperCase() + meal.complexity.name.substring(1);
+  }
+  String get affordabilityText{
+    return meal.affordability.name[0].toUpperCase() + meal.affordability.name.substring(1);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,7 +60,11 @@ class MealItem extends StatelessWidget {
                     const SizedBox(height: 12),
                     Row(
                       children: [
-                        
+                        MealItemTrait(icon: Icons.schedule, label: '${meal.duration} min'),
+                        const SizedBox(width: 12,),
+                        MealItemTrait(icon: Icons.work, label: complexityText),
+                        const SizedBox(width: 12,),
+                        MealItemTrait(icon: Icons.currency_rupee_outlined, label: affordabilityText),
                       ],
                     )
                   ],
