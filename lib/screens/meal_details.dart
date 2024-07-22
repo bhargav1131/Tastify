1b50e8768649cee47d05cf22b6831fc0e53645f6
Bhargav Pratim <bhargavsarma173@gmail.com>
2024-07-22 21:45:21 +0530

    improve the detail page and make is scrollable

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tastify/models/meal_mod.dart';
 
-class MealDetailsScreen extends StatelessWidget{
+class MealDetailsScreen extends StatelessWidget {
   const MealDetailsScreen({super.key, required this.meal});
 
   final Meal meal;
@@ -12,7 +12,51 @@ class MealDetailsScreen extends StatelessWidget{
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Image.network(meal.imageUrl, width: double.infinity, height: 280, fit: BoxFit.cover,),
+      body: SingleChildScrollView(
+        child: Column(
+          children:[
+              Image.network(
+              meal.imageUrl,
+              width: double.infinity,
+              height: 280,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 15,),
+            Text('Ingredients', 
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold
+                ),
+            ),
+            const SizedBox(height: 15,),
+        
+            //displaying the ingredients to prepare the meal
+            for(final ingredient in meal.ingredients)
+              Text(ingredient, style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                  color: Theme.of(context).colorScheme.onSurface,
+              ),
+            ),
+            const SizedBox(height: 23,),
+            Text('Steps', 
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold
+                ),
+            ),
+        
+            //displaying the steps to prepare the meal
+            for(final step in meal.steps)
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(step, textAlign: TextAlign.center ,style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onSurface,
+                ),
+                          ),
+              ),
+        
+          ],
+        ),
+      ),
     );
   }
 }
