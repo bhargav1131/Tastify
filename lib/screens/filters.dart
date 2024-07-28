161c82fa79562f1cc147a7455cef4173181ba462
Bhargav Pratim <bhargavsarma173@gmail.com>
2024-07-28 17:24:38 +0530

    pop the drawer upon clicking back arrow ffrom filter screen

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tastify/screens/tabs.dart';
+import 'package:tastify/widgets/main_drawer.dart';
 
 class FiltersScreen extends StatefulWidget{
   const FiltersScreen({super.key});
@@ -18,6 +20,12 @@ class _FiltersScreenState extends State<FiltersScreen>{
       appBar: AppBar(
         title: const Text('Your Filters'),
       ),
+      drawer: MainDrawer(onSelectScreen: (identifier){
+        Navigator.of(context).pop();
+        if(identifier == 'Meals'){
+          Navigator.of(context).push(MaterialPageRoute(builder: (ctx)=> const TabScreen()));
+        }
+      }),
       body: Column(
         children: [
           SwitchListTile(
