9c5fe43fe22fb631e492c250b532f51186b87dac
Bhargav Pratim <bhargavsarma173@gmail.com>
2024-08-02 21:17:57 +0530

    trigger the notifier on tapping fav button

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -1,64 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tastify/models/meal_mod.dart';
+import 'package:tastify/provider/favorites_provider.dart';
 
-class MealDetailsScreen extends StatelessWidget {
-  const MealDetailsScreen({super.key, required this.meal,});
+class MealDetailsScreen extends ConsumerWidget {
+  const MealDetailsScreen({
+    super.key,
+    required this.meal,
+  });
 
   final Meal meal;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
         actions: [
           IconButton(
-            onPressed: (){
-            }, icon: const Icon(Icons.star),),
+            onPressed: () {
+              final isAdded = ref
+                  .read(favoriteMealsProvider.notifier)
+                  .toggleMealFavoriteStatus(meal);
+
+              ScaffoldMessenger.of(context).clearSnackBars();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(isAdded
+                      ? "Meal added to favorites"
+                      : "Meal is removed from favorites"),
+                ),
+              );
+            },
+            icon: const Icon(Icons.star),
+          ),
         ],
       ),
       body: SingleChildScrollView(
         child: Column(
-          children:[
-              Image.network(
+          children: [
+            Image.network(
               meal.imageUrl,
               width: double.infinity,
               height: 280,
               fit: BoxFit.cover,
             ),
-            const SizedBox(height: 15,),
-            Text('Ingredients', 
-                style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                  color: Theme.of(context).colorScheme.primary,
-                  fontWeight: FontWeight.bold
-                ),
+            const SizedBox(
+              height: 15,
             ),
-            const SizedBox(height: 15,),
-        
+            Text(
+              'Ingredients',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+
             //displaying the ingredients to prepare the meal
-            for(final ingredient in meal.ingredients)
-              Text(ingredient, style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                  color: Theme.of(context).colorScheme.onSurface,
+            for (final ingredient in meal.ingredients)
+              Text(
+                ingredient,
+                style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onSurface,
+                    ),
               ),
+            const SizedBox(
+              height: 23,
             ),
-            const SizedBox(height: 23,),
-            Text('Steps', 
-                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+            Text(
+              'Steps',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
                   color: Theme.of(context).colorScheme.primary,
-                  fontWeight: FontWeight.bold
-                ),
+                  fontWeight: FontWeight.bold),
             ),
-        
+
             //displaying the steps to prepare the meal
-            for(final step in meal.steps)
+            for (final step in meal.steps)
               Padding(
                 padding: const EdgeInsets.all(10.0),
-                child: Text(step, textAlign: TextAlign.center ,style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                    color: Theme.of(context).colorScheme.onSurface,
+                child: Text(
+                  step,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                        color: Theme.of(context).colorScheme.onSurface,
+                      ),
                 ),
-                          ),
               ),
-        
           ],
         ),
       ),
