a7ad5721c5572679f5a15d592edb8504a89b145b
Bhargav Pratim <bhargavsarma173@gmail.com>
2024-08-02 21:15:59 +0530

    return bool instead of void

--- lib/provider/favorites_provider.dart
+++ lib/provider/favorites_provider.dart
@@ -4,13 +4,15 @@ import 'package:tastify/models/meal_mod.dart';
 class FavoriteMealsNotifier extends StateNotifier<List<Meal>>{
   FavoriteMealsNotifier(): super([]);
 
-  void toggleMealFavoriteStatus(Meal meal){
+  bool toggleMealFavoriteStatus(Meal meal){
     final mealIsFavorite = state.contains(meal);
 
     if(mealIsFavorite){
       state = state.where((meeal) => meeal.id != meal.id).toList();
+      return false;
     }else{
       state = [...state, meal];
+      return true;
     }
   }
 }
